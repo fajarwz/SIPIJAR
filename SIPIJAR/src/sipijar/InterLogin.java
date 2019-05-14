8e89bb282c72624c58f587614398191e8a648ee9
fajarwz <39845286+fajarwz@users.noreply.github.com>
2019-05-14 23:07:03 +0700

    Add files via upload

---
+++ SIPIJAR/src/sipijar/InterLogin.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sipijar;
+
+/**
+ *
+ * @author rifaldo
+ */
+public interface InterLogin {
+    public void cekLogin();
+    public String loginAs(String newQuery);
+}
